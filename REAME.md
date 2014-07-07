bb8030c59c961928d5f9e9b0f94cf66548580689
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-07-07 12:52:09 +0400

    Some functions and README update

--- REAME.md
+++ REAME.md
@@ -1,1 +1,5 @@
-# Intial
+# Create dirrectories
+
+	mkdir -p /var/lib/astor2-rrdd/{rrd,png}
+	mkdir -p /var/run/astor2-rrdd/
+	mkdir -p /var/log/astor2-rrdd/
--- rrd.py
+++ rrd.py
@@ -1,6 +1,9 @@
 import os.path
+# RRD module
+from rrdtool import *
 
-db_path = '/var/lib/astor2-rrdd/'
+db_path = '/var/lib/astor2-rrdd/rrd/'
+png_path = '/var/lib/astor2-rrdd/png/'
 
 def commit():
 	# RRD databases list
@@ -12,11 +15,31 @@ def commit():
 			# Code to update
 		else:
 			print('Not ok')
+			new_db(rrd, key)
 			# Code to create new db and write to it
 
 def check_db(db):
 	rrd_db = db_path + db
 	return os.path.isfile(rrd_db)
 
-def new_db(db):
-	rrd_db = db_path + db
+# Create new db
+def new_db(rrd, db_type):
+	rrd_db = db_path + rrd[db_type]
+	if db_type == 'net':
+		create_net(rrd_db)
+
+# Create new net DB
+def create_net(rrd_db):
+	fname = rrd_db
+	rrd = RoundRobinDatabase(fname)
+	# Create network database
+	rrd.create(
+		DataSource("in", type=DeriveDST, heartbeat=600, min=0, max=12500000),
+		DataSource("out", type=DeriveDST, heartbeat=600, min=0, max=12500000),
+		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=1, rows=576),
+		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=6, rows=672),
+		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=24, rows=732),
+		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=144, rows=1460),
+		step=300
+		)
+	print('Database created')
