7356719c90f4d3e6ce52d4051a7f96039ee470dd
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-07-07 12:24:30 +0400

    RRD module

--- rrd.py
+++ rrd.py
@@ -1,2 +1,12 @@
 def commit():
 	print('test')
+	# RRD databases list
+	rrd = {'memory' : 'Memory.rrd', 'net' : 'Network.rrd', 'cpu' : 'Cpu.rrd'}
+	for key in rrd.iteritems():
+		db_check = check_db(rrd[key])
+		if db_check == 0:
+			# Code to update
+
+def check_db(db):
+	db_path = '/var/lib/astor2-rrdd/'
+	rrd_db = db_path . db
