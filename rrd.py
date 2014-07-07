ac39275d502f278bc065a41c069b973fb7027cdc
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-07-07 13:00:00 +0400

    Rrd db create is working

--- rrd.py
+++ rrd.py
@@ -1,6 +1,8 @@
 import os.path
+import sys
+
 # RRD module
-from rrdtool import *
+import rrdtool
 
 db_path = '/var/lib/astor2-rrdd/rrd/'
 png_path = '/var/lib/astor2-rrdd/png/'
@@ -30,16 +32,13 @@ def new_db(rrd, db_type):
 
 # Create new net DB
 def create_net(rrd_db):
-	fname = rrd_db
-	rrd = RoundRobinDatabase(fname)
+	data_sources=[ 'DS:speed1:COUNTER:600:U:U',
+                'DS:speed2:COUNTER:600:U:U',
+                'DS:speed3:COUNTER:600:U:U' ]
 	# Create network database
-	rrd.create(
-		DataSource("in", type=DeriveDST, heartbeat=600, min=0, max=12500000),
-		DataSource("out", type=DeriveDST, heartbeat=600, min=0, max=12500000),
-		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=1, rows=576),
-		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=6, rows=672),
-		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=24, rows=732),
-		RoundRobinArchive(cf=AverageCF, xff=0.5, steps=144, rows=1460),
-		step=300
-		)
+	rrdtool.create( rrd_db,
+                 '--start', '920804400',
+                 data_sources,
+                 'RRA:AVERAGE:0.5:1:24',
+                 'RRA:AVERAGE:0.5:6:10' )
 	print('Database created')
