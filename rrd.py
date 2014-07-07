c2e62390372f008cd447bfeb1627ce12e0672ca7
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-07-07 14:31:49 +0400

    DB updating

--- rrd.py
+++ rrd.py
@@ -7,24 +7,66 @@ import rrdtool
 db_path = '/var/lib/astor2-rrdd/rrd/'
 png_path = '/var/lib/astor2-rrdd/png/'
 
-# Update existing rrd databse
+# Update existing rrd database
+# Main function
 def commit():
 	# RRD databases list
 	rrd = {'mem' : 'Memory.rrd', 'net' : 'Network.rrd', 'cpu' : 'Cpu.rrd'}
 	for key, value in rrd.iteritems():
 		db_check = check_db(value)
 		if db_check == True:
-			# Code to update
-			return('Database updated')
+			update_db(rrd, key)
 		else:
 			new_db(rrd, key)
-			print('Database created')
-			# Code to create new db and write to it
 
 # Check of rrd db file
 def check_db(db):
 	rrd_db = db_path + db
 	return os.path.isfile(rrd_db)
+
+# Update DB functions
+#####################
+
+# Get traffic value
+def get_traf(cmd):
+	import os
+	return os.system(cmd)
+
+# Update rrd database
+def update_db(rrd, db_type):
+	# Update network DB
+	if db_type == 'net':
+		rrd_db = db_path + rrd[db_type]
+		in_cmd = 'ifconfig p3p1 |grep bytes|cut -d":" -f2|cut -d" " -f1'
+		in_traf = get_traf(in_cmd)
+		out_cmd = 'ifconfig p3p1 |grep bytes|cut -d":" -f3|cut -d" " -f1'
+		out_traf = get_traf(out_cmd)
+		rrd_up_cmd = 'rrdtool update ' + rrd_db +' -t "in:out" "N:' + str(in_traf) + ':' + str(out_traf) + '"'
+		get_traf(rrd_up_cmd)
+		# Create graph
+		rrd_graph_cmd = "rrdtool graph " + png_path + db_type + ".png " + "-s -1" + db_type + " " + "-t traffic on" + in_traf + "::" + out_traf +
+				" --lazy" +
+				" -h 80 -w 600" +
+				" -l 0 " +
+				" -a PNG" +
+				" -v bytes/sec" +
+				" DEF:in=" + rrd_db + ":in:AVERAGE",
+				" DEF:out=" + rrd_db + ":out:AVERAGE",
+				" CDEF:out_neg=out,-1,*" +
+				" AREA:in#32CD32:Incoming" +
+				" LINE1:in#336600" +
+				' GPRINT:in:MAX:  Max\\: %5.1lf %s' +
+				' GPRINT:in:AVERAGE: Avg\\: %5.1lf %S' +
+				" GPRINT:in:LAST: Current\\: %5.1lf %Sbytes/sec\\n" +
+				" AREA:out_neg#4169E1:Outgoing" +
+				" LINE1:out_neg#0033CC" +
+				" GPRINT:out:MAX:  Max\\: %5.1lf %S" +
+				" GPRINT:out:AVERAGE: Avg\\: %5.1lf %S" +
+				" GPRINT:out:LAST: Current\\: %5.1lf %Sbytes/sec" +
+				" HRULE:0#000000";
+
+# End of update DB functions
+############################
 
 # rrdtool create functions
 # Needs to rewrite
