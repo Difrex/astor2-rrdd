64d399cd0e46f6344b836fbc40d9c5b8309bb127
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-07-07 13:35:58 +0400

    Logging

--- rrd.py
+++ rrd.py
@@ -14,16 +14,21 @@ def commit():
 	for key, value in rrd.iteritems():
 		db_check = check_db(value)
 		if db_check == True:
-			print('Ok')
 			# Code to update
+			return('Database updated')
 		else:
 			new_db(rrd, key)
 			print('Database created')
 			# Code to create new db and write to it
 
+# Check of rrd db file
 def check_db(db):
 	rrd_db = db_path + db
 	return os.path.isfile(rrd_db)
+
+# rrdtool create functions
+# Needs to rewrite
+##########################
 
 # Create new db
 def new_db(rrd, db_type):
@@ -35,21 +40,20 @@ def new_db(rrd, db_type):
 	elif db_type == 'cpu':
 		create_cpu(rrd_db)
 
-# rrdtool create functions
-# Needs to rewrite
-##########################
-
 # Create new network DB
 def create_net(rrd_db):
-	data_sources=[ 'DS:speed1:COUNTER:600:U:U',
-                'DS:speed2:COUNTER:600:U:U',
-                'DS:speed3:COUNTER:600:U:U' ]
+	data_sources=[ 'DS:in:DERIVE:600:0:12500000',
+                'DS:out:DERIVE:600:0:12500000'
+                ]
 	# Create network database
 	rrdtool.create( rrd_db,
                  '--start', '920804400',
                  data_sources,
-                 'RRA:AVERAGE:0.5:1:24',
-                 'RRA:AVERAGE:0.5:6:10' )
+                 'RRA:AVERAGE:0.5:1:576',
+                 'RRA:AVERAGE:0.5:6:672',
+                 'RRA:AVERAGE:0.5:24:732',
+                 'RRA:AVERAGE:0.5:144:1460'
+                )
 
 # Create new memory DB
 def create_mem(rrd_db):
