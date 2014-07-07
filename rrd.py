ed0591dd3e1776533f6c4791053546371b2fb134
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-07-07 12:38:11 +0400

    Check DB

--- rrd.py
+++ rrd.py
@@ -1,12 +1,22 @@
+import os.path
+
+db_path = '/var/lib/astor2-rrdd/'
+
 def commit():
-	print('test')
 	# RRD databases list
 	rrd = {'memory' : 'Memory.rrd', 'net' : 'Network.rrd', 'cpu' : 'Cpu.rrd'}
-	for key in rrd.iteritems():
-		db_check = check_db(rrd[key])
-		if db_check == 0:
+	for key, value in rrd.iteritems():
+		db_check = check_db(value)
+		if db_check == True:
+			print('Ok')
 			# Code to update
+		else:
+			print('Not ok')
+			# Code to create new db and write to it
 
 def check_db(db):
-	db_path = '/var/lib/astor2-rrdd/'
-	rrd_db = db_path . db
+	rrd_db = db_path + db
+	return os.path.isfile(rrd_db)
+
+def new_db(db):
+	rrd_db = db_path + db
