b26b7f901882237533fcf997531c0576b576f59c
Ilya Oleksiv <oleksivio@gmail.com>
2014-07-08 08:16:33 -0400

    Add autoscale in net graph

--- rrd.py
+++ rrd.py
@@ -11,45 +11,46 @@ png_path = '/var/lib/astor2-rrdd/png/'
 # Update existing rrd database
 # Main function
 def commit():
-	# RRD databases list
-	rrd = {'mem' : 'Memory.rrd', 'net' : 'Network.rrd', 'cpu' : 'Cpu.rrd'}
-	for key, value in rrd.iteritems():
-		db_check = check_db(value)
-		if db_check == True:
-			update_db(rrd, key)
-			graph(rrd, key)
-			# Create graph
-		else:
-			print('DB '+value+'created')
-			new_db(rrd, key)
+    # RRD databases list
+    rrd = {'mem' : 'Memory.rrd', 'net' : 'Network.rrd', 'cpu' : 'Cpu.rrd'}
+    for key, value in rrd.iteritems():
+        db_check = check_db(value)
+        if db_check == True:
+            update_db(rrd, key)
+            graph(rrd, key)
+            # Create graph
+        else:
+            print('DB '+value+'created')
+            new_db(rrd, key)
 
 # Check of rrd db file
 def check_db(db):
-	rrd_db = db_path + db
-	return os.path.isfile(rrd_db)
+    rrd_db = db_path + db
+    return os.path.isfile(rrd_db)
 
 # Update DB functions
 #####################
 
 # Get traffic value
 def get_traf(cmd):
-	import os
-	out = os.popen(cmd).read()
-	out = int(out.strip())/1024/1024
-	return out
+    import os
+    out = os.popen(cmd).read()
+    print out.strip()
+    out = int(out.strip())/1024/1024
+    return out
 
 # Update rrd database
 def update_db(rrd, db_type):
-	rrd_db = db_path + rrd[db_type]
-	# Update network DB
-	if db_type == 'net':
-		in_cmd = 'ifconfig p3p1 |grep bytes | grep RX | awk \'{print $5}\''
-		in_traf = get_traf(in_cmd)
-		out_cmd = 'ifconfig p3p1 |grep bytes | grep TX | awk \'{print $5}\''
-		out_traf = get_traf(out_cmd)
-		print('Updating DB')
-		# Update rrd db
-		ret = rrdtool.update(rrd_db, 'N:%s:%s' %(in_traf, out_traf))
+    rrd_db = db_path + rrd[db_type]
+    # Update network DB
+    if db_type == 'net':
+        in_cmd = 'ifconfig eth0 | grep bytes | grep RX | cut -d":" -f3 | cut -d" " -f1'
+        in_traf = get_traf(in_cmd)
+        out_cmd = 'ifconfig eth0 | grep bytes | grep TX | cut -d":" -f3 | cut -d" " -f1'
+        out_traf = get_traf(out_cmd)
+        print('Updating DB')
+        # Update rrd db
+        ret = rrdtool.update(rrd_db, 'N:%s:%s' %(in_traf, out_traf))
 
 # End of update DB functions
 ############################
@@ -58,21 +59,21 @@ def update_db(rrd, db_type):
 ########################
 
 def graph(rrd, db_type):
-	png = png_path + db_type + '.png'
-	db = db_path + rrd[db_type]
-	if db_type == 'net':
-		rrdtool.graph(png, '--start', 'end-120000s', '--width', '400',
-			"--vertical-label=Num",
-			'--watermark=OpenSAN', '-w 800',
-			"DEF:in="+ db +":in:AVERAGE",
-			"DEF:out="+ db +":out:AVERAGE",
-			"LINE1:in#0000FF:in\\r",
-			"LINE2:out#00FF00:out\\r")
-			# "GPRINT:in:AVERAGE:Avg in\: %6.0lf ",
-			# "GPRINT:out:MAX:Max in\: %6.0lf \\r",
-			# "GPRINT:in:AVERAGE:Avg out\: %6.0lf ",
-			# "GPRINT:out:MAX:Max out\: %6.0lf \\r")
-		print('Graph generated')
+    png = png_path + db_type + '.png'
+    db = db_path + rrd[db_type]
+    if db_type == 'net':
+        rrdtool.graph(png, '--start', 'end-120000s', '--width', '400',
+            "--vertical-label=Num", "-M",
+            '--watermark=OpenSAN2', '-w 800',
+            "DEF:in="+ db +":in:AVERAGE",
+            "DEF:out="+ db +":out:AVERAGE",
+            "LINE1:in#0000FF:in\\r",
+            "LINE2:out#00FF00:out\\r",
+            "GPRINT:in:AVERAGE:Avg in\: %6.0lf ",
+            "GPRINT:out:MAX:Max in\: %6.0lf \\r",
+            "GPRINT:in:AVERAGE:Avg out\: %6.0lf ",
+            "GPRINT:out:MAX:Max out\: %6.0lf \\r")
+        print('Graph generated')
 
 # End of create graph functions
 ###############################
@@ -83,21 +84,21 @@ def graph(rrd, db_type):
 
 # Create new db
 def new_db(rrd, db_type):
-	rrd_db = db_path + rrd[db_type]
-	if db_type == 'net':
-		create_net(rrd_db)
-	elif db_type == 'mem':
-		create_mem(rrd_db)
-	elif db_type == 'cpu':
-		create_cpu(rrd_db)
+    rrd_db = db_path + rrd[db_type]
+    if db_type == 'net':
+        create_net(rrd_db)
+    elif db_type == 'mem':
+        create_mem(rrd_db)
+    elif db_type == 'cpu':
+        create_cpu(rrd_db)
 
 # Create new network DB
 def create_net(rrd_db):
-	data_sources=[ 'DS:in:DERIVE:600:0:12500000',
+    data_sources=[ 'DS:in:DERIVE:600:0:12500000',
                 'DS:out:DERIVE:600:0:12500000'
                 ]
-	# Create network database
-	rrdtool.create( rrd_db,
+    # Create network database
+    rrdtool.create( rrd_db,
                  '--start', '920804400',
                  data_sources,
                  'RRA:AVERAGE:0.5:1:576',
@@ -108,11 +109,11 @@ def create_net(rrd_db):
 
 # Create new memory DB
 def create_mem(rrd_db):
-	data_sources=[ 'DS:speed1:COUNTER:600:U:U',
+    data_sources=[ 'DS:speed1:COUNTER:600:U:U',
                 'DS:speed2:COUNTER:600:U:U',
                 'DS:speed3:COUNTER:600:U:U' ]
-	# Create network database
-	rrdtool.create( rrd_db,
+    # Create network database
+    rrdtool.create( rrd_db,
                  '--start', '920804400',
                  data_sources,
                  'RRA:AVERAGE:0.5:1:24',
@@ -120,11 +121,11 @@ def create_mem(rrd_db):
 
 # Create new CPU DB
 def create_cpu(rrd_db):
-	data_sources=[ 'DS:speed1:COUNTER:600:U:U',
+    data_sources=[ 'DS:speed1:COUNTER:600:U:U',
                 'DS:speed2:COUNTER:600:U:U',
                 'DS:speed3:COUNTER:600:U:U' ]
-	# Create network database
-	rrdtool.create( rrd_db,
+    # Create network database
+    rrdtool.create( rrd_db,
                  '--start', '920804400',
                  data_sources,
                  'RRA:AVERAGE:0.5:1:24',
