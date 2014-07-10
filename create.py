716f88e7b461f5bdbc2c2e7ead676fa5fe7d0d7d
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-07-10 18:07:55 +0400

    All functions moved

--- create.py
+++ create.py
@@ -1,6 +1,31 @@
+from rrdsys import interfaces
+
+import os
+import os.path
+
+# RRD module
+import rrdtool
+
+db_path = '/var/lib/astor2-rrdd/rrd/'
+png_path = '/var/lib/astor2-rrdd/png/'
+
 # rrdtool create functions
 # Needs to rewrite
 ##########################
+
+# Check of rrd db file
+def check_db(rrd, db_type):
+    if db_type == 'net':
+        ifaces = interfaces()
+        for i in ifaces:
+            rrd_db = db_path + i + '.' + rrd[db_type]
+            if os.path.isfile(rrd_db) != True:
+                new_db(rrd, db_type)
+        return True
+    else:
+        rrd_db = db_path + rrd[db_type]
+        return os.path.isfile(rrd_db)
+
 
 # Create new db
 def new_db(rrd, db_type):
@@ -11,6 +36,7 @@ def new_db(rrd, db_type):
         create_mem(rrd_db)
     elif db_type == 'cpu':
         create_cpu(rrd)
+
 
 # Create new network DB
 def create_net(rrd):
@@ -28,6 +54,7 @@ def create_net(rrd):
                      'RRA:AVERAGE:0.5:10:1008',
                      'RRA:AVERAGE:0.5:10:1008',
                     )
+
 
 # Create new memory DB
 def create_mem(rrd_db):
--- rrd.py
+++ rrd.py
@@ -1,5 +1,6 @@
 from rrdsys import get_mem, get_traf, get_cmd, interfaces
 from update import *
+from create import new_db, check_db
 
 import os.path
 from os.path import join
@@ -13,94 +14,8 @@ def commit():
     # RRD databases list
     rrd = { 'mem': 'Memory.rrd', 'net': 'Network.rrd', 'cpu': 'Cpu.rrd' }
     for key, value in rrd.iteritems():
-        db_check = check_db(rrd, key)
-        if db_check == True:
-            update_db(rrd, key)
-        else:
-            print('DB '+value+' created')
+        if check_db(rrd, key) != True:
             new_db(rrd, key)
-
-# Check of rrd db file
-def check_db(rrd, db_type):
-    if db_type == 'net':
-        ifaces = interfaces()
-        for i in ifaces:
-            rrd_db = db_path + i + '.' + rrd[db_type]
-            if os.path.isfile(rrd_db) != True:
-                new_db(rrd, db_type)
-            else:
-                return True
-    else:
-        rrd_db = db_path + rrd[db_type]
-        return os.path.isfile(rrd_db)
-
-
-# rrdtool create functions
-# Needs to rewrite
-##########################
-
-# Create new db
-def new_db(rrd, db_type):
-    rrd_db = db_path + rrd[db_type]
-    if db_type == 'net':
-        create_net(rrd)
-    elif db_type == 'mem':
-        create_mem(rrd_db)
-    elif db_type == 'cpu':
-        create_cpu(rrd)
-
-# Create new network DB
-def create_net(rrd):
-    ifaces = interfaces()
-    for i in ifaces:
-        rrd_db = db_path + i + '.' + rrd['net']
-        data_sources=[ 'DS:in:COUNTER:120:0:U',
-                    'DS:out:COUNTER:120:0:U'
-                    ]
-        # Create network database
-        rrdtool.create( rrd_db,
-                     '--start', '920804400',
-                     data_sources,
-                     'RRA:AVERAGE:0.5:1:360',
-                     'RRA:AVERAGE:0.5:10:1008',
-                     'RRA:AVERAGE:0.5:10:1008',
-                    )
-
-# Create new memory DB
-def create_mem(rrd_db):
-    data_sources = [ 'DS:free:GAUGE:120:0:U',
-                'DS:total:GAUGE:120:0:U',
-                'DS:cached:GAUGE:120:0:U',
-                'DS:buffers:GAUGE:120:0:U',
-                'DS:used:GAUGE:120:0:U'
-                ]
-    # Create network database
-    rrdtool.create( rrd_db,
-                 data_sources,
-                 'RRA:AVERAGE:0.5:1:360',
-                 'RRA:AVERAGE:0.5:10:1008,',
-                 'RRA:MAX:0.5:10:1008'
-                  )
-
-# Create new CPU DB
-def create_cpu():
-    physicals = cpus()
-    cores = cpu_cores()
-
-    if physicals <= 1:
-        count = 0
-        while count < cores:
-            db = '/tmp/' + count + 'cpu.rrd'
-            data_sources=[ 'DS:speed1:COUNTER:600:U:U',
-                        'DS:speed2:COUNTER:600:U:U',
-                        'DS:speed3:COUNTER:600:U:U' ]
-            # Create network database
-            rrdtool.create( db,
-                         '--start', '920804400',
-                         data_sources,
-                         'RRA:AVERAGE:0.5:1:24',
-                         'RRA:AVERAGE:0.5:6:10' )
-            count = count + 1
-
-# End of rrdtool create functions
-#################################
+            print('DB '+value+' created')
+        else:
+            update_db(rrd, key)
