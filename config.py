fe63f16e6b9afe90ea60dd4d33a9879b27dcbf6d
Ilya Oleksiv <oleksivio@gmail.com>
2014-09-10 12:46:10 +0400

    Add conditions from config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # File with global settings variable
-CPU_ENABLED=1;
+CPU_ENABLED=0;
 MEM_ENABLED=1;
 NET_ENABLED=1;
 BATTERY_ENABLED=1;
--- rrd.py
+++ rrd.py
@@ -1,6 +1,7 @@
 from rrdsys import get_mem, get_traf, get_cmd, interfaces
 from update import *
 from create import new_db, check_db
+import config
 
 # RRD module
 import rrdtool
@@ -8,8 +9,17 @@ import rrdtool
 # Update existing rrd database
 # Main function
 def commit():
-    # RRD databases list
-    rrd = { 'mem': 'Memory.rrd', 'net': 'Network.rrd', 'cpu': 'Cpu.rrd' }
+
+    rrd={}
+    # Generate RRD databases list
+    if config.MEM_ENABLED:
+        rrd['mem']='Memory.rrd'
+    if config.CPU_ENABLED:
+        rrd['cpu']='Cpu.rrd'
+    if config.NET_ENABLED:
+        rrd['net']='Network.rrd'
+    print(rrd)
+
     for key, value in rrd.iteritems():
         if check_db(rrd, key) != True:
             new_db(rrd, key)
